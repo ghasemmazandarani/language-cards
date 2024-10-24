d5fdc1dac2d0648a0887c6ece54157e7bdd19f41
Ghasem Mazandarani <61150346+ghasemmazandarani@users.noreply.github.com>
2024-10-24 19:30:18 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import {RouterProvider,createBrowserRouter} from "react-router-dom";
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {path: '/' , element : <App />},
-
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
